02bdf9da154ab0c0fa9bbd29dbf1d7ea911f1ade
qreol <stim19255@gmail.com>
2024-03-26 20:16:05 +0300

    init

---
+++ src/main/java/qreol/project/keymarket/KeyMarketApplication.java
@@ -0,0 +1,13 @@
+package qreol.project.keymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/qreol/project/keymarket/KeyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package qreol.project.keymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
